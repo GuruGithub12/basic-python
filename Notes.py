90175853d829905609dcc2e081103dca363a99a0
Guru <gurug.qa@gmail.com>
2026-04-29 16:16:10 +1000

    my first commit to release branch

---
+++ Notes.py
@@ -0,0 +1,18 @@
+# Key Difference (One‑liner)
+# Loop Type	i Means	Use When
+# for i in range(len(prices))	 i = index ----- use when you need the index
+# for i in prices	i = value Cleaner, preferred when you don’t need index
+# from http.client import CONTINUE
+
+
+# BREAK STOPS A FOR  LOOP ENTIRELY
+# CONTINUE SKIPS THE CURRENT ITERATION AND MOVES TO THE NEXT ONE
+
+
+# IN LIST MY_LIST
+# MY_LIST[-1] GIVES THE LAST ELEMENT
+# MY_LIST[-2] GIVES THE SECOND LAST ELEMENT
+# my_LIST[0] GIVES THE FIRST ELEMENT
+# MY_LIST[1:4] GIVES ELEMENTS FROM INDEX 1 TO 3 (4 NOT INCLUDED)
+
+print("this is the notes i made for myself to understand the concepts better and to have a quick reference for the future")
